144e2b5bdd7cbe7e5e4b5c4e46d67d17b674a369
= <radlohead@naver.com>
2019-05-25 10:54:02 +0900

    express and rest api add

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const twilio = require('twilio')
+const express = require('express')
+const app = express()
 const userInfo = require('./userInfo')
 const accountSid = userInfo.accountSid
 const authToken = userInfo.authToken
 const client = twilio(accountSid, authToken)
 
-client.messages
-    .create({
-        body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-        from: '+12562903533',
-        to: '+821075457456'
-    })
-    .then(message => console.log(message.sid))
+app.get('/', (req, res) => {
+    res.send('hi twilio sms')
+})
 
-console.log('twilio: ', client)
+app.get('/receive', (req, res) => {
+    client.messages
+        .create({
+            body:
+                'This is the ship that made the Kessel Run in fourteen parsecs?',
+            from: '+12562903533',
+            to: '+821075457456'
+        })
+        .then(message => {
+            res.send('sms send success')
+        })
+})
+
+app.listen(3000, () => console.log('twilio server listening on port 3000'))
+// console.log('twilio: ', client)
