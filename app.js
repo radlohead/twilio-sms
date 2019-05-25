eb8255e411452696d511c37503a8149f284b92f9
= <radlohead@naver.com>
2019-05-25 13:33:26 +0900

    simple sms send service apply

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
 const twilio = require('twilio')
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const userInfo = require('./userInfo')
 const accountSid = userInfo.accountSid
 const authToken = userInfo.authToken
 const client = twilio(accountSid, authToken)
 
+app.set('/views', __dirname + '/index.html')
+app.set('view engine', 'ejs')
+app.engine('html', require('ejs').renderFile)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
-    res.send('hi twilio sms')
+    res.render('index.html')
 })
 
-app.get('/receive', (req, res) => {
+app.get('/sms/success', (req, res) => {
+    res.send('sms send success')
+})
+
+app.post('/receive', async (req, res) => {
     client.messages
         .create({
-            body:
-                'This is the ship that made the Kessel Run in fourteen parsecs?',
+            body: req.body.smsInput,
             from: '+12562903533',
             to: '+821075457456'
         })
         .then(message => {
-            res.send('sms send success')
+            res.redirect('http://localhost:3000/sms/success')
         })
 })
 
 app.listen(3000, () => console.log('twilio server listening on port 3000'))
-// console.log('twilio: ', client)
